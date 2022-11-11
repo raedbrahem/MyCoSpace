2d0b8e570c84e9db198f6d66294a6ecfcd88fc38
raedbrahem <raed.brahem@esprit.tn>
2022-11-11 08:45:21 +0100

    mainwin.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "dialog.h"
 #include <QMainWindow>
 #include <employe.h>
+#include <QFileDialog>
 namespace Ui {
 class MainWindow;
 }
@@ -18,17 +19,35 @@ public:
 private slots:
     void on_pb_ajouter_clicked();
     void on_pb_supprimer_clicked();
-
     void on_pb_delete_clicked();
-
     void on_list_employes_currentRowChanged(int currentRow);
-
     void on_pb_update_clicked();
+    void on_pb_chercher_clicked();
+    void on_pb_trier_clicked();
+
+    void on_pb_pdf_clicked();
+
+    void on_tabWidget_2_tabBarClicked(int index);
+
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
+    void on_pb_trier_nom_clicked();
+
+    void on_pb_trier_cin_clicked();
+
+    void on_tab_etudiants_activated(const QModelIndex &index);
+
+    void clearLayout(QLayout *layout);
+
+
+    void on_pb_image_clicked();
+
+    void on_pb_imagemaj_clicked();
 
 private:
     Ui::MainWindow *ui;
     Employe E;
-    int mnSelected=-1;
 };
+
 
 #endif // MAINWINDOW_H
