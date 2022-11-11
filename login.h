26ea2b82caf58ff805c7e3ad6f9335aeb9ccafdb
raedbrahem <raed.brahem@esprit.tn>
2022-11-11 08:40:00 +0100

    login.h

--- login.h
+++ login.h
@@ -1,11 +1,16 @@
 #ifndef LOGIN_H
 #define LOGIN_H
-
+#include <QString>
 
 class login
 {
 public:
     login();
+    bool createuser();
+    void connect();
+private:
+    QString username;
+    QString password;
 };
 
 #endif // LOGIN_H
