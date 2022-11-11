e59a1a71ab14f6ea5501df492aa8ef61cb8ab528
raedbrahem <raed.brahem@esprit.tn>
2022-11-11 08:39:52 +0100

    login.cpp

--- login.cpp
+++ login.cpp
@@ -1,6 +1,19 @@
 #include "login.h"
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include<QtDebug>
+#include<QString>
 
 login::login()
 {
 
+}
+bool login::createuser()
+{
+    QSqlQuery query;
+    query.prepare("insert into users(username,password)"
+                  "VALUES(:username,:password)");
+    query.bindValue(":username",username);
+    query.bindValue(":pw",password);
+    return query.exec();
 }
