5e61275f72f48ae27170f446aef21e931d8e1524
raedbrahem <raed.brahem@esprit.tn>
2022-11-13 10:06:01 +0100

    login.cpp

--- login.h
+++ login.h
@@ -1,13 +1,20 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 #include <QString>
-
+#include <QSqlQueryModel>
 class login
 {
 public:
     login();
+    login(QString un,QString pw){this->username=un;
+                                this->password=pw;}
     bool createuser();
     void connect();
+    QSqlQueryModel *afficher();
+    bool modifier();
+    bool supprimer(QString);
+    void setusername(QString un){this->username =un;}
+    QString getusername(){return this->username;}
 private:
     QString username;
     QString password;
